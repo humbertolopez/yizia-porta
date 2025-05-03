b68bbbd4d19b5042e83953fc9fd186b9fc163f93
Humberto López <humberto@373.mx>
2025-05-02 18:22:43 -0600

    added tags, fixed images

--- components/general/Card/Card.tsx
+++ components/general/Card/Card.tsx
@@ -24,14 +24,16 @@ export default function Card({
   linkUrl,
   linkLabel,
   tagline,
+  tags,
 }: CardProps) {
   return (
-    <div className="w-1/3">
+    <div className="w-1/3 relative">
       <Image
         src={imageSrc}
         alt={imageAlt}
         width={imageWidth}
         height={imageHeight}
+        className="w-full h-auto"
       />
       <div className="p-10">
         {tagline && (
@@ -39,6 +41,18 @@ export default function Card({
         )}
         <h3 className="font-poppins text-3xl mb-3">{title}</h3>
         <p className="font-signika text-base mb-4">{description}</p>
+        {tags && (
+          <div className="flex gap-2 mb-4">
+            {tags.map((tag, index) => (
+              <span
+                key={index}
+                className="px-2.5 py-1 rounded-sm border border-gray-300 text-sm font-signika font-semibold"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        )}
         <Link
           href={linkUrl}
           className="font-signika font-medium after:content-['>'] after:ml-4"
